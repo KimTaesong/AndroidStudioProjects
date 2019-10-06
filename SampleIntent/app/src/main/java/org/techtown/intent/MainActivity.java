2e886fc6a45ace42697a387d72f93023958440de
KimTaesong <48849867+KimTaesong@users.noreply.github.com>
2019-10-06 21:08:37 +0900

    Initial commit

---
+++ SampleIntent/app/src/main/java/org/techtown/intent/MainActivity.java
@@ -0,0 +1,44 @@
+package org.techtown.intent;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivityForResult(intent, 101);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 101) {
+            Toast.makeText(this,"onActivityResult 호출됨: " + requestCode+"," + resultCode,Toast.LENGTH_LONG).show();
+
+            if (resultCode == RESULT_OK){
+                String name = data.getStringExtra("name");
+                Toast.makeText(this,"응답으로 받은 name: ", Toast.LENGTH_LONG).show();
+
+            }
+        }
+    }
+
+}
---
+++ Test2/app/src/main/java/org/techtown/test/MainActivity.java
@@ -0,0 +1,17 @@
+package org.techtown.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }// 코드블럭 -> 영역을 구분
+
+
+}
